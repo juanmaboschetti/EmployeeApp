41692600ed98cd4b65750e19686eb8fa7a1ff17a
Juan Boschetti <Juan.Boschetti@ey.com>
2020-10-25 20:06:33 -0300

    "New Changes"

---
+++ EmployeeApp.Business/Models/IEmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeApp.Business.Models
+{
+    public interface IEmployeeDto
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int RoleId { get; set; }
+        string RoleName { get; set; }
+        string RoleDescription { get; set; }
+        double HourlySalary { get; set; }
+        double MonthlySalary { get; set; }
+        double AnualSalary { get; set; }
+
+    }
+}
---
+++ EmployeeApp.Business/Models/MonthlySalaryEmployeeDto.cs
@@ -0,0 +1,24 @@
+﻿namespace EmployeeApp.Business.Models
+{
+    class MonthlySalaryEmployeeDto : IEmployeeDto
+    {
+        public MonthlySalaryEmployeeDto(int id, double monthlySalary, string name, int roleId, string roleDescription, string roleName)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.RoleId = roleId;
+            this.RoleDescription = roleDescription;
+            this.RoleName = roleName;
+            this.MonthlySalary = monthlySalary;
+            this.AnualSalary = monthlySalary * 12;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public double HourlySalary { get; set; }
+        public double MonthlySalary { get; set; }
+        public double AnualSalary { get; set; }
+    }
+}
